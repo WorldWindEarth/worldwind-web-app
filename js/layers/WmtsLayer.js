3eae1279becdd341310f68ad21b9c872196c1a43
Bruce Schubert <bruce@emxsys.com>
2018-12-12 17:47:54 -0800

    Added WmtsLayer class.
    - Simplifies WMTS layer creation.

---
+++ js/layers/WmtsLayer.js
@@ -0,0 +1,83 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2018 Bruce Schubert.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+/*global $, WorldWind, $ */
+
+/**
+ * The USGS TNM Topo Base Map layer.
+ * 
+ * See: https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/WMTS/1.0.0/WMTSCapabilities.xml
+ * 
+ * @returns {UsgsTopoBaseMapLayer}
+ */
+
+export default class WmtsLayer extends WorldWind.Layer {
+    constructor(serviceAddress, layerIdentifier, displayName) {
+        super(displayName);
+        let self = this;
+
+        // Called asynchronously to parse and create the WMTS layer
+        let createLayer = function (xmlDom) {
+            // Create a WmtsCapabilities object from the XML DOM
+            var wmtsCapabilities = new WorldWind.WmtsCapabilities(xmlDom);
+            // Retrieve a WmtsLayerCapabilities object by the desired layer name
+            var wmtsLayerCapabilities = wmtsCapabilities.getLayer(layerIdentifier);
+            // Form a configuration object from the WmtsLayerCapabilities object
+            var wmtsConfig = WorldWind.WmtsLayer.formLayerConfiguration(wmtsLayerCapabilities);
+            // Create the WMTS Layer from the configuration object
+            self.wmtsLayer = new WorldWind.WmtsLayer(wmtsConfig);
+        };
+
+        // Called if an error occurs during WMTS Capabilities document retrieval
+        let logError = function (jqXhr, text, exception) {
+            console.log("There was a failure retrieving the capabilities document: " + text + " exception: " + exception);
+        };
+
+        $.get(serviceAddress).done(createLayer).fail(logError);
+    }
+
+    /**
+     * Subclass method called to display this layer. Subclasses should implement this method rather than the
+     * [render]{@link Layer#render} method, which determines enable, pick and active altitude status and does not
+     * call this doRender method if the layer should not be displayed.
+     * @param {DrawContext} dc The current draw context.
+     * @protected
+     */
+    doRender(dc) {
+        return this.wmtsLayer.doRender(dc);
+    }
+    /**
+     * Indicates whether this layer is within the current view. Subclasses may override this method and
+     * when called determine whether the layer contents are visible in the current view frustum. The default
+     * implementation always returns true.
+     * @param {DrawContext} dc The current draw context.
+     * @returns {boolean} true If this layer is within the current view, otherwise false.
+     * @protected
+     */
+    isLayerInView(dc) {
+        return this.wmtsLayer ? this.wmtsLayer.isLayerInView(dc) : false;
+    }
+
+};
